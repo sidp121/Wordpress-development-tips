af65892e58389ed2e1802556901cf556ab6de5ca
Siddharth Pandey <134340488+sidp121@users.noreply.github.com>
2023-05-31 03:07:09 +0530

    Update Open-popup-click-on-the-Logo.php

--- Open-popup-click-on-the-Logo.php
+++ Open-popup-click-on-the-Logo.php
@@ -16,17 +16,11 @@ function popup_portfolio_company(){
           
                 <div class="tlp-col-md-4 tlp-col-sm-6 tlp-col-xs-12 tlp-single-item tlp-grid-item tlp-equal-height">
                     <div class="tlp-portfolio-item"> 
-                        <div class="tlp-portfolio-thum tlp-item">
+                        <div class="tlp-portfolio-thum tlp-item" id="popupcompany1" class="overlay">
 							<?php  $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');?> 
                             <a href="#">
-                                <img decoding="async" width="300" height="60" src="<?php echo $featured_img_url; ?>" class="img-responsive overlay" id="popupcompany1" alt="">
-                            </a>    
-                          <div class="tlp-overlay">
-				          <p class="link-icon">
-					     <a class="tlp-zoom" href="<?php echo $featured_img_url; ?>"><i class="demo-icon icon-zoom-in"></i></a>
-					     <a href="#" id="popupcompany1" class="overlay"><i class="demo-icon icon-link-ext"></i></a>
-				        </p>
-			            </div>    
+                                <img decoding="async" width="300" height="60" src="<?php echo $featured_img_url; ?>" class="img-responsive" alt="">
+                            </a>        
                         </div>
                         <!-- <div id="popupcompany1" class="overlay">
                         <div class="popup"> -->
@@ -34,8 +28,8 @@ function popup_portfolio_company(){
                             <div class="tlp-content-holder">
                                 <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>        
                             </div>
-                        </div>
-                       <!--  </div>
+                         </div>
+                      <!--   </div>
                     </div> -->
                 </div>
             </div>
@@ -51,3 +45,5 @@ function popup_portfolio_company(){
 
     return ob_get_clean(); 
 }
+
+?>
